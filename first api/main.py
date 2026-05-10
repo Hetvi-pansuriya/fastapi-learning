45f602a70924b45914bb3f596ed0011ec4044a5c
Hetvi pansuriya <hetvipansuriya75@gmail.com>
2026-05-10 20:50:40 +0530

    add path parameter and DELETE route

--- first api/main.py
+++ first api/main.py
@@ -25,4 +25,14 @@ def add_expenses(expense: Expense):
     expenses.append(expense.dict()) #convert the Expense object to a dictionary and add it to your list.
     return {"message": "Expense Added", "expense": expense}
 
+@app.get("/expenses/{category}") #{category}: path parameter
+def get_by_category(category:str): #FastAPI automatically takes the value from the URL and passes it to this function as category
+    result=[e for e in expenses if e["category"].lower()==category.lower()] # list comprehension, loop through every expenses of list, check if expense category matches with requested
+    return {"expenses":result}
 
+@app.delete("/expenses/{index}") #This defines a DELETE route, {index} is a path parameter
+def delete_expense(index:int): #FastAPI takes the number from the URL and passes it to this function as index
+    if index < len(expenses): # check if the index is valid
+        removed=expenses.pop(index) #pop(index) removes the item at that position from the lis
+        return {"message": "Expense Deleted", "expense": removed} 
+    return {"message" : "Invalid Index"}
